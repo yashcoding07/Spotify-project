a525b6957041dd54406c8692dc2cb150368ec6cd
yashcoding07 <yash07coding@gmail.com>
2026-04-20 12:06:52 +0530

    restart of the project

--- Backend/auth/server.js
+++
@@ -1,8 +0,0 @@
-import app from "./src/app.js";
-import connectDB from "./src/db/db.js";
-
-connectDB()
-
-app.listen(3000, () => {
-    console.log("Auth server is running on port 3000");
-})
--- Backend/auth/src/controllers/auth.controllers.js
+++
@@ -1,4 +0,0 @@
-import userModel from "../models/user.model";
-import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt";
-import config from "../config/config";
